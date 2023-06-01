460fda2b07d68b00526c55af29cbc034838fc355
Senne Knaepkens <senne.knaepkens@telenet.be>
2023-06-01 17:44:32 +0200

    Generation of app with Fiori Generator

---
+++ webapp/controller/DetailObjectNotFound.controller.js
@@ -0,0 +1,7 @@
+sap.ui.define([
+    "./BaseController"
+], function (BaseController) {
+    "use strict";
+
+    return BaseController.extend("movement.erpadvancedmovement.controller.DetailObjectNotFound", {});
+});
